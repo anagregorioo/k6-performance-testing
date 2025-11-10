a73ebd8d9c00bc1f6280c540b632993f75399f31
Leandro Ugioni <leandro_ugioni@hotmail.com>
2025-11-10 00:18:36 -0300

    Add dashboard HTML and update dependencies

--- src/tests/Tests.spec.js
+++ src/tests/Tests.spec.js
@@ -1,4 +1,4 @@
-import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
+import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/latest/dist/bundle.js';
 import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 import http from 'k6/http';
 import { check } from 'k6';
@@ -14,8 +14,9 @@ export const options = {
     content_OK: ['rate>0.95']
   },
   stages: [
-    { duration: '10s', target: 5 },
-    { duration: '20s', target: 15 }
+    { duration: '3s', target: 2 },
+    { duration: '3s', target: 6 },
+    { duration: '3s', target: 9 }
   ]
 };
 
